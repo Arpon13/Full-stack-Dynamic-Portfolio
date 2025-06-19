34b7bf2400b8f490f36784b987d8371e78f37944
Arpon13 <arpon15-5655@diu.edu.bd>
2025-06-19 14:33:52 +0600

    home page design

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
 </head>
 <body>
-    <p>Welcome to the Laravel application!</p>
+     <header class="site-header">
+  <div class="header-content">
+    <div class="logo">ARPON</div>
+    <nav class="navbar">
+      <a href="#" class="nav-item">Home</a>
+      <a href="#" class="nav-item">About</a>
+      <a href="#" class="nav-item">Projects</a>
+      <a href="#" class="nav-item">Contact</a>
+    </nav>
+  </div>
+</header>
+
+    <div class="container">
+    <img src="https://png.pngtree.com/png-clipart/20230927/original/pngtree-man-avatar-image-for-profile-png-image_13001882.png" alt="Your Name" class="profile-img">
+    <h1 class="name">Md Asif Shahriar Arpon</h1>
+    <p class="subtitle">Hello, I am a Data Sceintists with excellent skills with python, Scikit learn, SQl etc.</p>
+  </div>
 </body>
 </html>
